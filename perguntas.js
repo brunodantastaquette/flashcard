f5d0487d2b14a362e9d3489e598c4f983e3a7b0d
brunodantastaquette <taquette.bruno@escola.pr.gov.br>
2025-06-02 11:39:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'Qual é o maior animal do mundo?',
+'O maior animal do mundo é o elefante'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual era o nome do continente onde era tudo junto?',
+    'Seu nome era Pangeia'
+)
+
+criaCartao(
+    'história',
+    'Titanic afundou em qual foi o ano?',
+    'Afundou em 1912'
+)
+
+criaCartao(
+    'matemática',
+    'Oque é uma matriz?',
+    'Estrutura matemática com colunas e linhas números'
+)
